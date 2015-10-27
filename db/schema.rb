283a03e76329c250e4f4609933ef20960ecc24a9
Taylor Huston <taylor.a.huston@gmail.com>
2015-10-27 14:33:56 -0700

    Rename Make to Company

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027212754) do
+ActiveRecord::Schema.define(version: 20151027213239) do
 
   create_table "cars", force: :cascade do |t|
-    t.string   "make"
+    t.string   "company"
     t.string   "color"
     t.integer  "year"
     t.datetime "created_at",                          null: false
